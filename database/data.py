a6fddad61b54e9fe754baf57b0f65b5b9730bad8
NickNameYouTuber <74864374+NickNameYouTuber@users.noreply.github.com>
2023-03-25 22:06:02 +0300

    new database code

--- database/data.py
+++ database/data.py
@@ -8,7 +8,7 @@ def sql_start():
         print("Database is OK")
 
     #База
-    base.execute("""CREATE TABLE IF NOT EXISTS users (
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
         login TEXT, 
         password TEXT,
         cash BIGINT
@@ -20,9 +20,9 @@ def sql_start():
     user_login = input("Login: ")
     user_password = input("Password: ")
 
-    base.execute("SELECT login FROM users") #SELECT - выбрать "login", так же с помощью * можно выбрать все; FROM - в столбце "users"
-    if base.fetchone() is None:
-        base.execute("INSERT INTO users VALUES (?, ?, ?)", (user_login, user_password, 0))
+    cur.execute("SELECT login FROM users") #SELECT - выбрать "login", так же с помощью * можно выбрать все; FROM - в столбце "users"
+    if cur.fetchone() is None:
+        cur.execute("INSERT INTO users VALUES (?, ?, ?)", (user_login, user_password, 0))
         base.commit()
 
         print("Зарегистрировано!")
