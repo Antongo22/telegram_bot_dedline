549754ff410b00c51517c7aafaca9a518c0ab8ed
Mason-Zaccaro <65304987+Mason-Zaccaro@users.noreply.github.com>
2023-03-25 21:55:26 +0300

    Создал базу данных

--- database/data.py
+++ database/data.py
@@ -1,7 +1,27 @@
 import sqlite3
 
-data_base = sqlite3.connect('datab.db') # путь к базе данных
-cursor = data_base.cursor()
+# Создание соединения с базой данных
+db = sqlite3.connect('server.db')
+sql = db.cursor() #Курсор нужен для работы с баззой данных - удаление и добавление
+
+#База
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    login TEXT, 
+    password TEXT,
+    cash BIGINT
+)""") #BIGINT - Тип данных означающее число. От 0 до Сикстилионов
+
+db.commit() #Подтверждение действия (Создание таблицы)
 
 
-data_base.commit() # завершение работы
+user_login = input("Login: ")
+user_password = input("Password: ")
+
+sql.execute("SELECT login FROM users") #SELECT - выбрать "login", так же с помощью * можно выбрать все; FROM - в столбце "users"
+if sql.fetchone() is None:
+    sql.execute("INSERT INTO users VALUES (?, ?, ?)", (user_login, user_password, 0))
+    db.commit()
+
+    print("Зарегистрировано!")
+else:
+    print("Такая запись уже имеется")
