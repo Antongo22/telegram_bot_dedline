a2806e8ebd76dbcacd040479b4f1739284ad147e
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-09-24 18:36:20 +0300

    в

--- bot.py
+++ bot.py
@@ -3,10 +3,8 @@ from create_bot import dp
 from database.func import check_deadlines
 
 
-
 async def on_startup(_):  # Показать, что бот запущен
     print("Бот начал работать!")
-
 
 
 from telegram import main_commands, create_dedline, show_dedline, other, settings_time, settings_name, \
